c315c7d7441c98ec4d13ab1a0e75a55a8b13c812
NATAH <natexs25@gmail.com>
2017-10-10 10:09:40 -0500

    reinicializando

---
+++ app.js.js
@@ -0,0 +1,13 @@
+function string= prompt ("Hola! Escribe una frase");
+
+
+    var string = "ABCDEFGHIJKLMNOPEQURSTUVWXYZ";
+ 
+for (var i>=65; i <=90; i = i++) {
+    if((string.charCodeAt(i>=26) && (string.charCodeAt(i<=90)); 
+
+    result = string.charCodeAt(i)-65-code%33
+   
+}
+
+
