0174c09f9b0fe9b6b176ec8a86ff38106dbe423d
natah <32302934+natah@users.noreply.github.com>
2017-10-10 12:16:58 -0500

    Update app.js.js

--- app.js.js
+++ app.js.js
@@ -1,13 +1,25 @@
-function string= prompt ("Hola! Escribe una frase");
+var string= prompt ("Hola! Escribe una frase");
 
-
-    var string = "ABCDEFGHIJKLMNOPEQURSTUVWXYZ";
- 
-for (var i>=65; i <=90; i = i++) {
+function cipher(texto){  
+    
+    var phrase="abcdefghijklmnopqrstuvwxyz"
+    
+    var text=text.toUpperCase()  
+    
+    
+    for (var i>=65; i <=90; i = i++) {
     if((string.charCodeAt(i>=26) && (string.charCodeAt(i<=90)); 
+{
+    result = string.charCodeAt(i)-65-code%26; if(result)
+  
+  
+    }  
+    return r  
+}
 
-    result = string.charCodeAt(i)-65-code%33{
-       else 
+    var string = "";
+ 
+
    
 }
 
